6a5b3ed743b795850931b2127126f24138fe39ea
MrSn001 <shaheen36.saa@gmail.com>
2026-06-16 08:53:23 +0400

    Implemented MainMenu() Method

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -1,7 +1,48 @@
 ﻿namespace OOP_Part1
 {
+    
+
+
     internal class Program
     {
+        public void MainMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("""
+                ================================================
+                GRAND VISTA HOTEL — MANAGEMENT SYSTEM
+                ================================================
+                """);
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("""
+                1. Add New Room
+                2. Register New Guest
+                3. Book a Room for a Guest
+                """);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("""
+                4. Search & Filter Rooms
+                5. Guest & Booking Statistics
+                """);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("""
+                6. Check Out a Guest
+                7. Remove Unavailable Rooms
+                """);
+            
+            Console.ResetColor();
+            Console.WriteLine("0. Exit");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("""
+                ================================================
+                Enter your choice:
+                """);
+            Console.ResetColor();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
