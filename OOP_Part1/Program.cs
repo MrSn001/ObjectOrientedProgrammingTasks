1ba9668b7003206315ad3acd04f29b837180006a
MrSn001 <shaheen36.saa@gmail.com>
2026-06-16 10:49:39 +0400

    Implemented Class Guest

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -30,6 +30,24 @@ namespace OOP_Part1
 
     }
 
+    public class Guest
+    {
+        public int guestId;
+        public string guestName;
+        public int roomNumber;
+        public DateOnly checkInDate;
+        public int totalNights;
+
+        public Guest(int id, string name, int roomNum, DateOnly checkIn, int totalNights)
+        {
+            guestId = id;
+            guestName = name;
+            roomNumber = roomNum;
+            checkInDate = checkIn;
+            this.totalNights = totalNights;
+        }
+    }
+
     internal class Program
     {
         static int choice;
