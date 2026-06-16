abf749fc0249f6be4d655ba6ea8c664e5a82f06b
MrSn001 <shaheen36.saa@gmail.com>
2026-06-16 10:56:27 +0400

    Switched The Guest Class to new Class file named Guest.cs

---
+++ OOP_Part1/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Part1
+{
+    public class Guest
+    {
+        public int guestId;
+        public string guestName;
+        public int roomNumber;
+        public DateOnly checkInDate;
+        public int totalNights;
+
+        public Guest(int id, string name, int roomNum, DateOnly checkIn, int totalNights)
+        {
+            guestId = id;
+            guestName = name;
+            roomNumber = roomNum;
+            checkInDate = checkIn;
+            this.totalNights = totalNights;
+        }
+    }
+}
--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -2,28 +2,7 @@
 
 namespace OOP_Part1
 {
-    
-   
-
-    public class Guest
-    {
-        public int guestId;
-        public string guestName;
-        public int roomNumber;
-        public DateOnly checkInDate;
-        public int totalNights;
-
-        public Guest(int id, string name, int roomNum, DateOnly checkIn, int totalNights)
-        {
-            guestId = id;
-            guestName = name;
-            roomNumber = roomNum;
-            checkInDate = checkIn;
-            this.totalNights = totalNights;
-        }
-    }
-
-    internal class Program
+internal class Program
     {
         static int choice;
         static bool flag = true;
