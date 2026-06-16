4a5f6c6abd1700f48903495e52554300a84cfe0c
MrSn001 <shaheen36.saa@gmail.com>
2026-06-16 10:54:50 +0400

    Switched the Class Room to a new file Called Room.cs

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -3,32 +3,7 @@
 namespace OOP_Part1
 {
     
-    public class Room
-    {
-        public int roomNumber;
-        public string roomType;
-        public double pricePerNight;
-        public bool isAvailable;
-
-        public Room(int roomNum, string Type , double price , bool availability)
-        {
-            roomNumber = roomNum;
-            roomType = Type;
-            pricePerNight = price;
-            isAvailable = availability;
-        }
-
-        public void DisplayRoom()
-        {
-            Console.WriteLine($"""
-                Room Number: {roomNumber}
-                Room Type: {roomType}
-                Price Per Night: {pricePerNight}
-                Status: {isAvailable}
-                """);
-        }
-
-    }
+   
 
     public class Guest
     {
---
+++ OOP_Part1/Room.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Part1
+{
+    public class Room
+    {
+        public int roomNumber;
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvailable;
+
+        public Room(int roomNum, string Type, double price, bool availability)
+        {
+            roomNumber = roomNum;
+            roomType = Type;
+            pricePerNight = price;
+            isAvailable = availability;
+        }
+
+        public void DisplayRoom()
+        {
+            Console.WriteLine($"""
+                Room Number: {roomNumber}
+                Room Type: {roomType}
+                Price Per Night: {pricePerNight}
+                Status: {isAvailable}
+                """);
+        }
+    }
+}
